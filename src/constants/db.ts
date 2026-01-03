7414e16928ade8978fbcc3cbe038f2902c902aa7
Tendani Netshitenzhe <tbnetshitenzhe@gmail.com>
2026-01-03 20:30:42 +0200

    updated constants

---
+++ src/constants/db.ts
@@ -0,0 +1,1 @@
+export const MAX_DB_ENTRIES = 40;
---
+++ src/constants/hotels.ts
@@ -0,0 +1,9 @@
+import { hotels as osaka } from "./hotels/osaka";
+import { hotels as tokyo } from "./hotels/tokyo";
+import { hotels as seoul } from "./hotels/seoul";
+
+export const ALL_HOTELS = [
+  { type: "osaka", hotels: osaka },
+  { type: "tokyo", hotels: tokyo },
+  { type: "seoul", hotels: seoul },
+];
