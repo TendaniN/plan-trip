4fc5a58d56959029f52ef8ad647419b93b919949
Tendani Netshitenzhe <tbnetshitenzhe@gmail.com>
2026-01-11 20:35:57 +0200

    added trips list page

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Image } from "@mantine/core";
+import { Flex, Image, Title } from "@mantine/core";
 import { Link } from "react-router-dom";
 
 import { UnAuthenticated } from "./unauthenticated";
@@ -7,20 +7,34 @@ import { AuthWrapper } from "components";
 import logoImg from "assets/branding/logo-transparent.png";
 
 const boxStyle = {
-  display: "flex",
-  justifyContent: "space-between",
-  width: "100%",
-  padding: "0.5rem 1rem",
-  boxShadow: "0 2px 4px 0 rgba(0, 0, 0, 0.2)",
+  root: {
+    padding: "0.5rem 1rem",
+    boxShadow: "0 2px 4px 0 rgba(0, 0, 0, 0.2)",
+
+    "& a": {
+      color: "#000",
+      textDecoration: "none",
+      margin: "auto 0",
+
+      ":hover": {
+        textDecoration: "underline",
+      },
+    },
+  },
 };
 
 export const Navbar = () => (
-  <Box style={boxStyle}>
+  <Flex justify="space-between" w="100%" styles={boxStyle}>
     <Link to="/">
       <Image w="5rem" src={logoImg} />
     </Link>
+    <AuthWrapper renderDenied={null}>
+      <Link to="/trip">
+        <Title fz="h2">My Trips</Title>
+      </Link>
+    </AuthWrapper>
     <AuthWrapper renderDenied={<UnAuthenticated />}>
       <Authenticated />
     </AuthWrapper>
-  </Box>
+  </Flex>
 );
