d2779f2b9130fd2dc4dfa205306e5d4be643c5e6
Tendani Netshitenzhe <tbnetshitenzhe@gmail.com>
2026-02-08 19:08:29 +0200

    added help and trips to navbar component

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -5,6 +5,11 @@ import { UnAuthenticated } from "./unauthenticated";
 import { Authenticated } from "./authenticated";
 import { AuthWrapper, CurrencyDropdown } from "components";
 import logoImg from "assets/branding/logo-transparent.png";
+import {
+  FaCartFlatbedSuitcase,
+  FaMapLocationDot,
+  FaCircleQuestion,
+} from "react-icons/fa6";
 
 const boxStyle = {
   root: {
@@ -25,16 +30,23 @@ const boxStyle = {
 
 const linkStyle = {
   root: {
+    gap: "0.5rem",
     a: {
       fontWeight: 700,
-      padding: "6px 12px",
+      padding: "0.325rem 0.75rem",
       borderRadius: 12,
       border: "2px solid transparent",
       backgroundColor: "var(--mantine-color-blue-4)",
+      display: "flex",
+      gap: "0.35rem",
 
       ":hover": {
         borderColor: "#000",
         textDecoration: "none",
+      },
+
+      "& svg": {
+        margin: "auto 0",
       },
     },
   },
@@ -47,7 +59,18 @@ export const Navbar = () => (
     </Link>
     <AuthWrapper renderDenied={null}>
       <Flex w="33.33%" justify="center" styles={linkStyle}>
-        <Link to="/country">Countries</Link>
+        <Link to="/country">
+          <FaMapLocationDot />
+          Countries
+        </Link>
+        <Link to="/trip">
+          <FaCartFlatbedSuitcase />
+          My Trips
+        </Link>
+        <Link to="/help">
+          <FaCircleQuestion />
+          Help
+        </Link>
       </Flex>
     </AuthWrapper>
     <AuthWrapper renderDenied={<UnAuthenticated />}>
