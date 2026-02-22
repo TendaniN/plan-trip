8e4ba958048b0eb3d53f1825078f675c59daba89
Tendani Netshitenzhe <tbnetshitenzhe@gmail.com>
2026-02-22 13:08:41 +0200

    removed custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/plan-trip/",
   resolve: {
     alias: {
       api: "/src/api",
