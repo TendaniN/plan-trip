9100ea51e7140919f673978c05aff95f65c42f99
Tendani Netshitenzhe <tbnetshitenzhe@gmail.com>
2026-04-06 18:08:43 +0200

    added table header constants

---
+++ src/constants/headers.ts
@@ -0,0 +1,116 @@
+export const LocationsTableHeader = [
+  {
+    id: "city",
+    label: "City",
+  },
+  {
+    id: "arrive",
+    label: "Arrive",
+  },
+  {
+    id: "depart",
+    label: "Depart",
+  },
+  {
+    id: "accommodation",
+    label: "Accommodation",
+  },
+  {
+    id: "nights",
+    label: "Nights",
+  },
+  {
+    id: "button",
+    label: "",
+  },
+  {
+    id: "remove",
+    label: "",
+  },
+];
+
+export const ItineraryTableHeader = [
+  {
+    id: "collapse",
+    label: "",
+  },
+  {
+    id: "date",
+    label: "Date",
+  },
+  {
+    id: "activity",
+    label: "Activity",
+  },
+  {
+    id: "time",
+    label: "Time",
+  },
+  {
+    id: "link",
+    label: "Link",
+  },
+  {
+    id: "cost",
+    label: "Cost",
+  },
+  {
+    id: "duration",
+    label: "Duration",
+  },
+  {
+    id: "remove",
+    label: "",
+  },
+];
+
+export const BudgetTableHeader = [
+  {
+    id: "Name",
+    label: "Name",
+  },
+  {
+    id: "type",
+    label: "Type",
+  },
+  {
+    id: "cost",
+    label: "Cost *",
+  },
+  {
+    id: "timespan",
+    label: "Timespan (now - start)",
+  },
+
+  {
+    id: "monthly",
+    label: "Monthly (cost / timespan) *",
+  },
+  {
+    id: "remove",
+    label: "",
+  },
+];
+
+export const AccommodationTableHeader = [
+  {
+    id: "index",
+    label: "",
+  },
+  {
+    id: "name",
+    label: "Name",
+  },
+  {
+    id: "rating",
+    label: "Rating",
+  },
+  {
+    id: "link",
+    label: "Link",
+  },
+  {
+    id: "collapse",
+    label: "",
+  },
+];
